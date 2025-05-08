82545a820ae9628d62ffaa6f2b0e134ae0225e87
otcheskiy <sa@osergey.ru>
2025-05-08 12:11:02 +0300

    вывод товара на главной

--- catalog/models.py
+++ catalog/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    code_1c = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'catalog/product_list.html', {'products': products})
